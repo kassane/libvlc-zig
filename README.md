70d8dca7a08007a5d3ec260b3668172bbbba3539
Matheus C. França <matheus-catarino@hotmail.com>
2023-03-26 11:52:36 -0300

    vlcpp support + example

--- README.md
+++ README.md
@@ -23,7 +23,7 @@
 
 # libvlc-zig
 
-Zig bindings for libVLC media framework.
+Zig bindings for libVLC media framework. Some of the features provided by libVLC include the ability to play local files and network streams, as well as to transcode media content into different formats. It also provides support for a wide range of codecs, including popular formats like H.264, MPEG-4, and AAC.
 
 ## Requirements
 
@@ -38,6 +38,59 @@ Zig bindings for libVLC media framework.
 $> zig build run -Doptimize=ReleaseSafe # print-version (default)
 $> zig build run -DExample="cli-player" -Doptimize=ReleaseSafe -- -i /path/multimedia_file
 ```
+
+## How to contribute to the libvlc-zig project?
+
+Read [Contributing](CONTRIBUTING.md).
+
+
+## FAQ
+
+
+### Q: Why isn't libvlc-zig licensed under **LGPLv2.1**?
+
+
+A: The decision to license `libvlc-zig` under the **BSD-2 clause** was made by the author of the project. This license was chosen because it allows for more permissive use and distribution of the code, while still ensuring that the original author receives credit for their work.
+
+`libvlc-zig` respects the original **LGPLv2.1** (Lesser General Public License) license of the VLC project.
+
+
+### Q: Are you connected to the developers of the original project?
+
+
+A: No, the author of `libvlc-zig` is not part of the **VideoLAN development team**. They are simply interested in being part of the VLC community and contributing to its development.
+
+
+### Q: What is the main goal of this project?
+
+
+A: The main goal of `libvlc-zig` is to provide a set of bindings for the **VLC media player's** libvlc library that are written in the **Zig programming language**. The project aims to provide a more modern and safe way to interface with the library, while maintaining compatibility with existing code written in **C** and **C++**.
+
+
+### Q: Does libvlc-zig aim to replace libvlc?
+
+
+A: No, `libvlc-zig` does not aim to replace libvlc. Instead, it provides an alternative way to interface with the library that may be more suitable for Zig developers.
+
+
+### Q: Can I use libvlc-zig in my project?
+
+
+A: Yes, you can use `libvlc-zig` in your project as long as you comply with the terms of the **BSD-2 clause** license. This includes giving credit to the original author of the code.
+
+
+### Q: Does libvlc-zig support all of the features of libvlc?
+
+
+A: `libvlc-zig` aims to provide bindings for all of the features of libvlc, but it may not be complete or up-to-date with the latest version of the library. If you encounter any missing features or bugs, please report them to the project's GitHub issues page.
+
+
+### Q: What programming languages are compatible with libvlc-zig?
+
+
+A: `libvlc-zig` provides bindings for the **Zig programming language**, but it can also be used with **C** and **C++** projects that use the libvlc library.
+
+
 
 ## License
 
--- build.zig
+++ build.zig
@@ -6,37 +6,55 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const examples = b.option([]const u8, "Example", "Build example: [print-version, cli-player, sdl2-player]") orelse "print-version";
+    const examples = b.option(
+        []const u8,
+        "Example",
+        "Build example: [print-version, cliPlayer-(c,cpp,zig), sdl2-player]",
+    ) orelse "print-version";
     if (std.mem.eql(u8, examples, "print-version"))
         make_example(b, .{
             .sdl_enabled = false,
+            .filetype = .zig,
             .mode = optimize,
             .target = target,
             .name = "print_version",
             .path = "examples/print_version.zig",
         });
 
-    if (std.mem.eql(u8, examples, "cli-player"))
+    if (std.mem.eql(u8, examples, "cliPlayer-zig"))
         make_example(b, .{
             .sdl_enabled = false,
+            .filetype = .zig,
             .mode = optimize,
             .target = target,
-            .name = "cli-player",
+            .name = "cliPlayer-zig",
             .path = "examples/cli_player.zig",
         });
 
-    if (std.mem.eql(u8, examples, "cli-player-c"))
-        make_Cexample(b, .{
+    if (std.mem.eql(u8, examples, "cliPlayer-c"))
+        make_example(b, .{
             .sdl_enabled = false,
+            .filetype = .c,
             .mode = optimize,
             .target = target,
-            .name = "cli-player-c",
+            .name = "cliPlayer-c",
             .path = "c_examples/cli_player.c",
+        });
+
+    if (std.mem.eql(u8, examples, "cliPlayer-cpp"))
+        make_example(b, .{
+            .sdl_enabled = false,
+            .filetype = .cpp,
+            .mode = optimize,
+            .target = target,
+            .name = "cliPlayer-cpp",
+            .path = "c_examples/cli_player.cpp",
         });
 
     if (std.mem.eql(u8, examples, "sdl-player")) {
         make_example(b, .{
             .sdl_enabled = true,
+            .filetype = .zig,
             .mode = optimize,
             .target = target,
             .name = "sdl-player",
@@ -46,18 +64,37 @@ pub fn build(b: *std.Build) void {
 }
 
 fn make_example(b: *std.Build, info: BuildInfo) void {
-    const example = b.addExecutable(.{
-        .name = info.name,
-        .target = info.target,
-        .optimize = info.mode,
-        .root_source_file = .{ .path = info.path },
-    });
+    const example = switch (info.filetype) {
+        .c, .cpp => b.addExecutable(.{
+            .name = info.name,
+            .target = info.target,
+            .optimize = info.mode,
+        }),
+        else => b.addExecutable(.{
+            .name = info.name,
+            .target = info.target,
+            .optimize = info.mode,
+            .root_source_file = .{ .path = info.path },
+        }),
+    };
+
+    if (info.mode != .Debug or info.mode != .ReleaseSafe) {
+        example.strip = true;
+        example.disable_sanitize_c = true;
+    } else example.bundle_compiler_rt = true;
 
     example.addAnonymousModule("vlc", .{
         .source_file = .{
             .path = "src/vlc.zig",
         },
     });
+
+    if (info.filetype == .c or info.filetype == .cpp)
+        example.addCSourceFile(info.path, &.{
+            "-Wall",
+            "-Werror",
+            "-Wextra",
+        });
 
     if (info.sdl_enabled) {
         const libsdl_dep = b.dependency("libsdl", .{
@@ -69,15 +106,26 @@ fn make_example(b: *std.Build, info: BuildInfo) void {
         example.installLibraryHeaders(libsdl);
     }
 
+    if (info.filetype == .cpp) {
+        const libvlcpp_dep = b.dependency("libvlcpp", .{
+            .target = info.target,
+            .optimize = info.mode,
+        });
+        const libvlcpp = libvlcpp_dep.artifact("vlcpp");
+        example.installLibraryHeaders(libvlcpp);
+        example.addIncludePath("zig-out/include");
+    }
+
     if (info.target.isDarwin()) {
         // Custom path
         example.addIncludePath("/usr/local/include");
         example.addLibraryPath("/usr/local/lib");
+
         // Link Frameworks
         example.linkFramework("Foundation");
         example.linkFramework("Cocoa");
         example.linkFramework("IOKit");
-        // example.linkFramework("Sparkle");
+
         // Link library
         example.linkSystemLibrary("vlc");
     } else if (info.target.isWindows()) {
@@ -89,7 +137,10 @@ fn make_example(b: *std.Build, info: BuildInfo) void {
     } else {
         example.linkSystemLibrary("vlc");
     }
-    example.linkLibC();
+    if (info.filetype == .cpp)
+        example.linkLibCpp()
+    else
+        example.linkLibC();
     example.install();
 
     const run_cmd = example.run();
@@ -99,67 +150,6 @@ fn make_example(b: *std.Build, info: BuildInfo) void {
     }
 
     var descr = b.fmt("Run the {s} example", .{info.name});
-    const run_step = b.step("run", descr);
-    run_step.dependOn(&run_cmd.step);
-}
-
-fn make_Cexample(b: *std.Build, info: BuildInfo) void {
-    const example = b.addExecutable(.{
-        .name = info.name,
-        .target = info.target,
-        .optimize = info.mode,
-    });
-    example.disable_sanitize_c = true;
-    example.addCSourceFile(info.path, &.{
-        "-Wall",
-        "-Wextra",
-    });
-    example.addAnonymousModule("vlc", .{
-        .source_file = .{
-            .path = "src/vlc.zig",
-        },
-    });
-
-    if (info.sdl_enabled) {
-        const libsdl_dep = b.dependency("libsdl", .{
-            .target = info.target,
-            .optimize = info.mode,
-        });
-        const libsdl = libsdl_dep.artifact("sdl");
-        example.linkLibrary(libsdl);
-        example.installLibraryHeaders(libsdl);
-    }
-
-    if (info.target.isDarwin()) {
-        // Custom path
-        example.addIncludePath("/usr/local/include");
-        example.addLibraryPath("/usr/local/lib");
-        // Link Frameworks
-        example.linkFramework("Foundation");
-        example.linkFramework("Cocoa");
-        example.linkFramework("IOKit");
-        // example.linkFramework("Sparkle");
-        // Link library
-        example.linkSystemLibrary("vlc");
-    } else if (info.target.isWindows()) {
-        // msys2/clang - CI
-        example.addIncludePath(msys2Inc(info.target));
-        example.addLibraryPath(msys2Lib(info.target));
-        example.linkSystemLibraryName("vlc.dll");
-        example.want_lto = false;
-    } else {
-        example.linkSystemLibrary("vlc");
-    }
-    example.linkLibC();
-    example.install();
-
-    const run_cmd = example.run();
-    run_cmd.step.dependOn(b.getInstallStep());
-    if (b.args) |args| {
-        run_cmd.addArgs(args);
-    }
-
-    var descr = b.fmt("Run the {s} - C example", .{info.name});
     const run_step = b.step("run", descr);
     run_step.dependOn(&run_cmd.step);
 }
@@ -178,11 +168,17 @@ fn checkVersion() bool {
 
 const BuildInfo = struct {
     sdl_enabled: bool,
-    // capy_enabled: bool, //TODO
+    filetype: SourceType,
     mode: std.builtin.Mode,
     target: std.zig.CrossTarget,
     name: []const u8,
     path: []const u8,
+};
+
+const SourceType = enum(u32) {
+    zig,
+    c,
+    cpp,
 };
 
 fn msys2Inc(target: std.zig.CrossTarget) []const u8 {
